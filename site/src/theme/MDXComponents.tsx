07f515990e1ede265d2c516803c833a652e21903
lshw54 <lshw.5454@gmail.com>
2026-09-11 17:11:22 +0800

    docs(site): show both ways into the client manager

--- site/src/theme/MDXComponents.tsx
+++ site/src/theme/MDXComponents.tsx
@@ -13,6 +13,7 @@ import DemoSessions from "../demo/DemoSessions";
 import DemoWebLaunch from "../demo/DemoWebLaunch";
 import DemoLoginError from "../demo/DemoLoginError";
 import DemoNodeErrors from "../demo/DemoNodeErrors";
+import DemoClientEntry from "../demo/DemoClientEntry";
 import DemoClientManager from "../demo/DemoClientManager";
 
 /** Components the MDX pages can use without importing them. */
@@ -32,5 +33,6 @@ export default {
   DemoWebLaunch,
   DemoLoginError,
   DemoNodeErrors,
+  DemoClientEntry,
   DemoClientManager,
 };
